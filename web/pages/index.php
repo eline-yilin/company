1d8174f94f2d7a970d21d91354dfef85ff0b5ebe
AndyZheng <tianhua.zheng@theoutnet.cn>
2014-11-17 10:55:46 +0800

    cnotact form init

--- web/pages/index.php
+++ web/pages/index.php
@@ -29,7 +29,37 @@ c</div>
 	<div class='my-bg'>
     		<img src="<?php echo THEME_PATH;?>images/nav-bg.jpg" />
     </div>
-d</div>
+    <div class='form-container'>
+    	<form class="form-horizontal" role="form">
+		  <div class="form-group">
+		    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+		    <div class="col-sm-10">
+		      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+		    </div>
+		  </div>
+		  <div class="form-group">
+		    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+		    <div class="col-sm-10">
+		      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+		    </div>
+		  </div>
+		  <div class="form-group">
+		    <div class="col-sm-offset-2 col-sm-10">
+		      <div class="checkbox">
+		        <label>
+		          <input type="checkbox"> Remember me
+		        </label>
+		      </div>
+		    </div>
+		  </div>
+		  <div class="form-group">
+		    <div class="col-sm-offset-2 col-sm-10">
+		      <button type="submit" class="btn btn-default">Sign in</button>
+		    </div>
+		  </div>
+		</form>
+    </div>
+</div>
 <?php
 include '../includes/footer.php';
 ?>
